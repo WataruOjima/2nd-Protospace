b4ce49a14c2870338c0f708803ec52d326d7c4b6
WataruOjima <wataru.ojima20@gmail.com>
2021-01-10 13:49:04 +0900

    トップページで表示

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,9 @@ validates :title,      presence: true
 validates :catch_copy, presence: true
 validates :concept,    presence: true
 validates :image,      presence: true
-validates :user,       presence: true
 
 belongs_to :user
-has_many :comments
+has_many :comments, dependent: :destroy
 has_one_attached :image
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'posts#index'
-  resources :posts, only: [:index, :new, :create]
+  root to: 'posts#index'
+  resources :posts, only: [:new, :create]
 end
