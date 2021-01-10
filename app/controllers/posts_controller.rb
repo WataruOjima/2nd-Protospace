26fe930291b05268e2aaae7ff3d532ad5f9030d3
WataruOjima <wataru.ojima20@gmail.com>
2021-01-10 16:24:56 +0900

    削除機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,13 +27,19 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = @post = Post.find(params[:id])
+    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to post_path
     else
       render :edit 
     end  
   end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path
+  end 
 
   private
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ validates :concept,    presence: true
 validates :image,      presence: true
 
 belongs_to :user
-has_many :comments, dependent: :destroy
+has_many :comments
 has_one_attached :image
 
 end
